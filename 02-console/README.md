1715f0b5ef3c7d19df40544ad2fc83392bb3c02f
Alexey Ermakov <strangebrew@yandex.ru>
2022-03-15 20:10:10 +0300

    Guess the number

--- 02-console/README.md
+++ 02-console/README.md
@@ -21,3 +21,11 @@ You can also run `npm link` to be able to use `simple-date` bin.
 `simple-date current`
 
 and so on.
+
+# quiz.js
+
+Try to guess the number.
+
+Type a number between 0 and 100 and press Enter.
+
+You can also run `npm link` to be able to use `simple-quiz` bin.
---
+++ 02-console/quiz.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+console.log("Загадано число в диапазоне от 0 до 100");
+
+const secretNum = Math.trunc(Math.random(0, 1) * 100) + 1;
+
+rl.on("line", (data) => {
+	const num = Number(data);
+
+	if (isNaN(num) || num < 0 || num > 100) {
+		console.log("Введите число от 0 до 100");
+		return;
+	}
+
+	if (secretNum > num) {
+		console.log("Больше");
+	} else if (secretNum < num) {
+		console.log("Меньше");
+	} else {
+		console.log("Отгадано число", secretNum);
+		rl.close();
+	}
+});
+
+rl.on("close", () => console.log("Закончили игру"));
