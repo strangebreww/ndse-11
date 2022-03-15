05a54a0aff84f1c97e4890c34954d1cab382ee5f
Alexey Ermakov <strangebrew@yandex.ru>
2022-03-15 17:43:40 +0300

    Add and subtract date, refactor

---
+++ 02-console/README.md
@@ -0,0 +1,23 @@
+# date.js
+
+Simple date management util.
+
+To get help, type `./date-js <command> --help`.
+
+Available commands are: `current`, `add` and `sub`.
+
+Examples:
+
+`./date.js current`
+
+`./date.js current --month`
+
+`./date.js add -y 2 -m 4 -d 1`
+
+`./date.js sub -y 1`
+
+You can also run `npm link` to be able to use `simple-date` bin.
+
+`simple-date current`
+
+and so on.
--- 02-console/date.js
+++ 02-console/date.js
@@ -2,43 +2,126 @@
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 
-yargs(hideBin(process.argv)).command(
-	"current [--year|-y] [--month|-m] [--day|-d]",
-	"print current date in ISO format",
-	function (yargs) {
-		return yargs
-			.option("year", {
-				alias: "y",
-				describe: "Get year",
-				type: "boolean",
-			})
-			.option("month", {
-				alias: "m",
-				describe: "Get month",
-				type: "boolean",
-			})
-			.option("day", {
-				alias: "d",
-				describe: "Get day of the month",
-				type: "boolean",
-			});
+const handlers = {
+	year: {
+		get: Date.prototype.getFullYear,
+		set: Date.prototype.setFullYear,
 	},
-	function (argv) {
-		const handlers = {
-			year: Date.prototype.getFullYear,
-			month: Date.prototype.getMonth,
-			day: Date.prototype.getDate,
-		};
+	month: {
+		get: Date.prototype.getMonth,
+		set: Date.prototype.setMonth,
+	},
+	day: {
+		get: Date.prototype.getDate,
+		set: Date.prototype.setDate,
+	},
+};
 
-		const keys = Object.keys(handlers);
-		const today = new Date();
+const options = Object.keys(handlers);
 
-		console.log("current date", today.toISOString());
+yargs(hideBin(process.argv))
+	.command(
+		"current [--year|-y] [--month|-m] [--day|-d]",
+		"print current date in ISO format",
+		function (yargs) {
+			return yargs
+				.option("year", {
+					alias: "y",
+					describe: "Get year",
+					type: "boolean",
+				})
+				.option("month", {
+					alias: "m",
+					describe: "Get month",
+					type: "boolean",
+				})
+				.option("day", {
+					alias: "d",
+					describe: "Get day of the month",
+					type: "boolean",
+				});
+		},
+		function (argv) {
+			const today = new Date();
 
-		for (const datePart in argv) {
-			if (keys.includes(datePart)) {
-				console.log(datePart, handlers[datePart].call(today));
+			console.log("current date", today.toISOString());
+
+			for (const datePart in argv) {
+				if (options.includes(datePart)) {
+					console.log(datePart, handlers[datePart].get.call(today));
+				}
 			}
 		}
-	}
-).argv;
+	)
+	.command(
+		"add [--year|-y] [--month|-m] [--day|-d]",
+		"add date and print",
+		function (yargs) {
+			return yargs
+				.option("year", {
+					alias: "y",
+					describe: "Add years",
+					type: "number",
+				})
+				.option("month", {
+					alias: "m",
+					describe: "Add months",
+					type: "number",
+				})
+				.option("day", {
+					alias: "d",
+					describe: "Add days",
+					type: "number",
+				});
+		},
+		function (argv) {
+			const resultDate = new Date();
+
+			for (const datePart in argv) {
+				if (options.includes(datePart)) {
+					handlers[datePart].set.call(
+						resultDate,
+						handlers[datePart].get.call(resultDate) + argv[datePart]
+					);
+				}
+			}
+
+			console.log("date in the future", resultDate.toISOString());
+		}
+	)
+	.command(
+		"sub [--year|-y] [--month|-m] [--day|-d]",
+		"subtract date and print",
+		function (yargs) {
+			return yargs
+				.option("year", {
+					alias: "y",
+					describe: "Subtract years",
+					type: "number",
+				})
+				.option("month", {
+					alias: "m",
+					describe: "Subtract months",
+					type: "number",
+				})
+				.option("day", {
+					alias: "d",
+					describe: "Subtract days",
+					type: "number",
+				});
+		},
+		function (argv) {
+			const resultDate = new Date();
+
+			for (const datePart in argv) {
+				if (options.includes(datePart)) {
+					handlers[datePart].set.call(
+						resultDate,
+						handlers[datePart].get.call(resultDate) - argv[datePart]
+					);
+				}
+			}
+
+			console.log("date in the past", resultDate.toISOString());
+		}
+	).argv;
