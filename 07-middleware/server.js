ebe36672b1cf1f13b47636b1e33405d703730f05
Alexey Ermakov <strangebrew@yandex.ru>
2022-04-16 15:07:36 +0300

    Add error middleware

--- 07-middleware/server.js
+++ 07-middleware/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const errorMiddleware = require("./middleware/error");
 const userRouter = require("./router/user");
 const booksRouter = require("./router/books");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
 app.use("/api/books", booksRouter);
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
