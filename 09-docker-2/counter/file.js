027589cb521d3dd5ff121d00d06df702858588d6
Alexey Ermakov <strangebrew@yandex.ru>
2022-05-24 22:28:35 +0300

    Share counter data volume between services

--- 09-docker-2/counter/file.js
+++ 09-docker-2/counter/file.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const fsPromises = require("fs/promises");
 const path = require("path");
 
-const dataDir = path.join(__dirname, "..", "data");
+const dataDir = path.join(__dirname, "..", "app-data");
 const file = path.join(dataDir, "counter.txt");
 
 function initCounter() {
