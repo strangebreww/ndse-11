892115ff03f83c8adba310e0793d296c6b081760
Alexey Ermakov <strangebrew@yandex.ru>
2022-06-26 23:39:47 +0300

    Add passport authentication

--- 12-auth/library/routes/api/user.js
+++ 12-auth/library/routes/api/user.js
@@ -1,11 +1,44 @@
 const router = require("express").Router();
+const passport = require("passport");
 
-router.post("/login", (_req, res) => {
-	res.status(201).json({ id: 1, mail: "test@mail.ru" });
+router.get("/login", function (req, res) {
+	res.render("user/login");
 });
+
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		failureRedirect: "/api/user/login",
+	}),
+	function (req, res) {
+		console.log("req.user: ", req.user);
+		res.redirect("/");
+	}
+);
 
 router.post("/signup", (_req, res) => {
 	res.status(201).json({ id: 1, mail: "test@mail.ru" });
 });
+
+router.get(
+	"/me",
+	function (req, res, next) {
+		if (!req.isAuthenticated || !req.isAuthenticated()) {
+			if (req.session) {
+				req.session.returnTo = req.originalUrl || req.url;
+			}
+			return res.redirect("/api/user/login");
+		}
+		next();
+	},
+	function (req, res) {
+		res.render("user/profile", { user: req.user });
+	}
+);
+
+// router.get("/logout", function (req, res) {
+// 	req.logout();
+// 	res.redirect("/");
+// });
 
 module.exports = router;
--- 12-auth/library/server.js
+++ 12-auth/library/server.js
@@ -1,31 +1,84 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const errorMiddleware = require("./middleware/error");
 const indexRouter = require("./routes/index");
 const booksRouter = require("./routes/books");
-const userRouter = require("./routes/user");
 const userApiRouter = require("./routes/api/user");
 const booksApiRouter = require("./routes/api/books");
+const expressSession = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("./db");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+	expressSession({
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/books", booksRouter);
-app.use("/user", userRouter);
 app.use("/api/user", userApiRouter);
 app.use("/api/books", booksApiRouter);
 
 app.use(errorMiddleware);
 
+function verify(username, password, done) {
+	console.log("verify");
+	db.users.findByUsername(username, function (err, user) {
+		if (err) {
+			return done(err);
+		}
+		if (!user) {
+			return done(null, false);
+		}
+
+		if (!db.users.verifyPassword(user, password)) {
+			return done(null, false);
+		}
+
+		return done(null, user);
+	});
+}
+
+const options = {
+	usernameField: "username",
+	passwordField: "password",
+	passReqToCallback: false,
+};
+
+passport.use("local", new LocalStrategy(options, verify));
+
+passport.serializeUser(function (user, cb) {
+	cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+	db.users.findById(id, function (err, user) {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, user);
+	});
+});
+
 const port = process.env.PORT || 3000;
-const dbName = process.env.DB_NAME || "library_service";
+// const dbName = process.env.DB_NAME || "library_service";
 
 async function start() {
 	try {
-		const UrlDb = `mongodb://mongo:27017/${dbName}`;
-		await mongoose.connect(UrlDb);
+		// const UrlDb = `mongodb://mongo:27017/${dbName}`;
+		// await mongoose.connect(UrlDb);
 
 		app.listen(port, () => {
 			console.log(`Server started on port ${port}`);
